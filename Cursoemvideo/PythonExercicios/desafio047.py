3d15b82a1429a6f3b751593bc1bc12e3713b8b4f
Mateus Filipe <mateuswk@outlook.com>
2024-04-26 13:48:35 -0300

    Adicionando exercicios

---
+++ Cursoemvideo/PythonExercicios/desafio047.py
@@ -0,0 +1,5 @@
+pares = 51
+
+for c in range(1, pares):
+    if c % 2 == 0:
+        print(c)
---
+++ Cursoemvideo/PythonExercicios/desafio048.py
@@ -0,0 +1,9 @@
+co = 0
+cont = 0
+
+for c in range(1, 501, 2):
+    if c % 3 == 0:
+        cont += 1
+        co += c
+
+print('\nA soma de todos os {} valores é {}\n'.format(cont, co))
