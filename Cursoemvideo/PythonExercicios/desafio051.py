375e7127f1296d637936fd0e0aeb3f3fe1eaf2f5
Mateus Filipe <mateuswk@outlook.com>
2024-04-29 23:04:11 -0300

    Adicionando Desafios

--- Cursoemvideo/PythonExercicios/desafio051.py
+++ Cursoemvideo/PythonExercicios/desafio051.py
@@ -1,9 +1,6 @@
-
-p = int(input('Primeiro termo '))
-r = int(input('Segundo termo '))
-soma = 0
-for c in range(p, r):
-    if r == r:
-        soma += r
-        print(soma)
-
+primeiro = int(input('Primeiro termo: '))
+razão = int(input('Razão: '))
+décimo = primeiro + (10 - 1) * razão
+for c in range(primeiro, décimo + razão, razão):
+    print('{} '.format(c), end='-> ')
+print('ACABOU')
